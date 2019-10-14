799a5a57f1fbcfe1b01cd11c7a59ac61ba852f91
shuxinhuang <shuxinhuang@users.noreply.github.com>
2019-10-14 10:47:10 +0000

    Deploying to gh-pages from master 461fcb9d4e2acdc932695fbe9b4ede0a9e4e6bcd

---
+++ precache-manifest.57d504e7de9c63133f6fd67805a6b106.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f7d7ebc92e399c2161f1d90511810b3",
+    "url": "/github-actions-demo/index.html"
+  },
+  {
+    "revision": "5d6ccc41178749de9a96",
+    "url": "/github-actions-demo/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "d4b6983ca5123f0760b2",
+    "url": "/github-actions-demo/static/js/2.a4040a16.chunk.js"
+  },
+  {
+    "revision": "5d6ccc41178749de9a96",
+    "url": "/github-actions-demo/static/js/main.31958c7c.chunk.js"
+  },
+  {
+    "revision": "72d09437806840199dbf",
+    "url": "/github-actions-demo/static/js/runtime~main.7e74eaf7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
